6bf5ce9f918bcb75f006a83fa7ed52709c4b0523
Dieter Plaetinck <dieter@kangaroot.net>
2010-10-04 16:38:43 +0200

    Remove cruft
    Remove:
    - old commented out code
    - pointless comments and code

--- sync-sugar-to-zimbra.php
+++ sync-sugar-to-zimbra.php
@@ -9,16 +9,11 @@
 // http://www.sugarcrm.com/wiki/index.php?title=SOAP_Intro_and_Practical_Examples
 // https://www.sugarcrm.com/forums/showthread.php?p=88449
 
-///////// GLOBALS /////////////////////////////////////////////////////////
-// this needs to be set for?? ref: http://www.sugarcrm.com/forums/showthread.php?t=27101
-// gives "Not a valid entrypoint" otherwise
-if(!defined('sugarEntry')) define('sugarEntry', true);  
 
-///////// INCLUDES ////////////////////////////////////////////////////////
+if(!defined('sugarEntry')) define('sugarEntry', true);
 require_once('zimbra-nusoap.php');
 require_once('config.php');
 
-///////// BODY ////////////////////////////////////////////////////////////
 
 function die_error ($msg, $exit = 2) {
 	if(is_array($msg)) {
@@ -28,28 +23,22 @@ function die_error ($msg, $exit = 2) {
 	exit ($exit);
 }
 
-// progress indicator
 echo("=> Connecting to SugarCRM\n");
-
-// modified to use nusoapclient based on http://www.sugarcrm.com/forums/showthread.php?t=27101
-//$client = new soapclient('http://www.example.com/sugar/soap.php?wsdl',true);
 $soapClient = new nusoapclient($sugarURL.'/soap.php?wsdl', true, $proxyHost, $proxyPort);
 
-// error check borrowed from: https://www.sugarcrm.com/forums/showthread.php?p=88449
 $err = $soapClient->getError();
 if ($err) {
 	die_error($err);
 }
-// do auth
+
+echo("=> Logging in to SugarCRM\n");
+
 $auth_array = array(
 	'user_auth' => array(
 		'user_name' => $sugarUser,
 		'password' => md5($sugarPwd),
 	)
 );
-
-// progress indicator
-echo("=> Logging in to SugarCRM\n");
 
 $login_results = $soapClient->call('login',$auth_array);
 $err = $soapClient->getError();
@@ -58,11 +47,9 @@ if ($err) {
 }
 
 $session =  $login_results['id'];
-//$user_guid = $soapClient->call('get_user_id',$session_id);
 
 try {
 
-  // progress indicator
   echo("=> Pulling contacts from SugarCRM\n");
 
   $result = $soapClient->call('get_entry_list',array('session'=>$session, 'module_name'=>'Contacts', 'query'=>'', 'order_by'=>'contacts.first_name asc', 'offset'=>0, 'select_fields'=>array(), 'max_results'=>100000));
@@ -70,12 +57,10 @@ try {
 	if ($err) {
 		die_error($err);
 	}
-  //$contacts = (array) $result->entry_list;
   $contacts = $result[entry_list];
 
   $fp = fopen( "/tmp/SugarCRMContacts.csv" , "w" );
 
-  // progress indicator
   echo("=> Parsing SugarCRM data into CSV for Zimbra\n");
 
   fwrite($fp, '"assistantPhone","birthday","callbackPhone","carPhone","company","companyPhone","description","department","dlist","email","email2","email3","fileAs","firstName","fullName","homeCity","homeCountry","homeFax","homePhone","homePhone2","homePostalCode","homeState","homeStreet","homeURL","initials","jobTitle","lastName","middleName","mobilePhone","namePrefix","nameSuffix","nickname","notes","office","otherCity","otherCountry","otherFax","otherPhone","otherPostalCode","otherState","otherStreet","otherURL","pager","workCity","workCountry","workFax","workPhone","workPhone2","workPostalCode","workState","workStreet","workURL","type"');
@@ -83,9 +68,8 @@ try {
 
   for ($c=0; $c<sizeOf($contacts); $c++) {
     $contact = $contacts[$c];
-    //$elements = (array) $contact->name_value_list;
     $elements = $contact[name_value_list];
-    
+
     $account_id = '';
     $account_name = '';
     $alt_address_city = '';
@@ -199,32 +183,23 @@ try {
   }
   fclose($fp);
 
-  // progress indicator
-  echo("=> Checking for debug mode\n");
-
-  // check for debug mode and print only if so
   if ($DEBUG === true) {
   		echo("===== DEBUG MODE IS ON - not changing data, just printing out the commands =====\n");
   		// clear out existing contacts
   		echo('=> Clear Zimbra:    /opt/zimbra/bin/zmprov sm '.$zimbra_account.' emptyFolder \'/'.$zimbra_folder.'\''."\n");
   		// add in the contacts harvested from sugar
   		echo('=> Add Sugar Data:  /usr/bin/curl -k -u '.$zimbra_username.':'.$zimbra_password.' -T @/tmp/SugarCRMContacts.csv "'.$zimbra_url.'/zimbra/home/'.$zimbra_account.'/'.urlencode($zimbra_folder).'?fmt=csv"'."\n");
-  } else if ($DEBUG === false) {
-        // progress indicator
+  } {
         echo("=> Clearing Zimbra Data\n");
-  		// clear out existing contacts
   		system('/opt/zimbra/bin/zmprov sm '.$zimbra_account.' emptyFolder \'/'.$zimbra_folder.'\'');
-  		// progress indicator
         echo("=> Adding parsed data from SugarCRM into Zimbra\n");
-  		// add in the contacts harvested from sugar
 		// curl is pretty lame. it might exit(0) even though the server gave http 400, luckily we can parse the output
 		exec  ('/usr/bin/curl -k -u '.$zimbra_username.':'.$zimbra_password.' -T /tmp/SugarCRMContacts.csv "'.$zimbra_url.'/zimbra/home/'.$zimbra_account.'/'.urlencode($zimbra_folder).'?fmt=csv"', $output);
 		$output = implode ($output,"\n");
 		if (stripos($output,'error') !== false) {
 			die_error($output);
 		}
-}//IF
-// progress indicator
+}
 echo("=> Done!\n");
 
 }
@@ -240,4 +215,3 @@ function my_replace($data) {
 
 unset($soapClient);
 ?>
-
